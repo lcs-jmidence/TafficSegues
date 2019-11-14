4bc7a8f43abf04f424aeb7463745864c1c0626a0
Julio Midence <jmidence@lcs.on.ca>
2019-11-13 20:43:13 -0500

    Made the switch work and the navigation controllers

--- TafficSegues/ViewController.swift
+++ TafficSegues/ViewController.swift
@@ -11,29 +11,32 @@ import UIKit
 class ViewController: UIViewController {
     
     // Mark: Properties / Outlets
-    
-    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var segueSwitch: UISwitch!
     
     
     //Mark: Initializer
     
     //Mark: Methods
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
-        
-    }
 
-    // Get ready to segue to the yellow view controller
-    // Is called automatically when a segue is about to happen
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
         
-        segue.destination.navigationItem.title = textField.text
     }
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    
+    
     
 }
 
