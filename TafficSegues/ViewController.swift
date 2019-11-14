c150c7850720fe6b34866b00fd87fbd71b9c8935
Julio Midence <jmidence@lcs.on.ca>
2019-11-13 19:18:12 -0500

    Initial Commit

---
+++ TafficSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TafficSegues
+//
+//  Created by Midence Rosario, Julio on 2019-11-13.
+//  Copyright © 2019 Julio Midence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
